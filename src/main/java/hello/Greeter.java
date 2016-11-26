437a2010fb7593955dfa597efc754d2ce61fcb3c
irwansyahrir <irwan.syahrir@gmail.com>
2016-11-26 08:25:05 +0100

    Initial commit. This is intended as a kata practice for refactoring, java 8, etc

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by irwansyahrir on 26/11/16.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Created by irwansyahrir on 26/11/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
