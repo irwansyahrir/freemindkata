95b682ba5568b7cd6f15fa639bf07061c31318d1
irwansyahrir <irwan.syahrir@gmail.com>
2016-11-29 18:50:18 +0100

    Added a simple test

---
+++ src/test/java/freemind/main/FreeMindTest.java
@@ -0,0 +1,40 @@
+package freemind.main;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.io.File;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+/**
+ * Created by irwansyahrir on 29/11/16.
+ */
+public class FreeMindTest {
+    private FrameFixture window;
+
+    @Before
+    public void setUp() {
+        Properties defaultProp = new Properties();
+
+        Properties userProp = new Properties();
+        userProp.setProperty("properties_folder", ".");
+
+        File autoPropFile = new File("");
+        FreeMind frame = GuiActionRunner.execute(() -> new FreeMind(defaultProp, userProp, autoPropFile));
+        window = new FrameFixture(frame);
+        window.show(); // shows the frame to test
+    }
+
+    @Test
+    public void testInitial() throws Exception {
+        Frame freeMind = window.target();
+        assertEquals("FreeMind", freeMind.getTitle());
+    }
+}
